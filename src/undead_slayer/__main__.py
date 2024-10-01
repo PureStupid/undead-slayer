107aa464365e346d76c15200b26e8d5a5fa665af
Pure <111236541+PureStupid@users.noreply.github.com>
2024-10-02 00:36:07 +0800

    Refactor __main__.py: Import UI enum and remove unused imports

--- src/undead_slayer/__main__.py
+++ src/undead_slayer/__main__.py
@@ -4,8 +4,9 @@ from typing import Annotated
 
 import typer
 
+from undead_slayer.enums import UI
 from undead_slayer.localization.language_manager import localization
-from undead_slayer.ui.ui_manager import UI, get_default_ui, launch_ui
+from undead_slayer.ui.ui_manager import get_default_ui, launch_ui
 
 _ = localization.language.gettext
 app = typer.Typer(help=_("An RPG Game!"), no_args_is_help=True)
@@ -14,7 +15,7 @@ app = typer.Typer(help=_("An RPG Game!"), no_args_is_help=True)
 @app.command(
     short_help=_("Play the game."),
     help=_(
-        """Play the game, with either a simple command-line (CLI),
+        """Play the game, with either a command-line (CLI),
         text (TUI), or graphical user interface (GUI)."""
     ),
 )
@@ -33,8 +34,20 @@ def play(
     launch_ui(ui)
 
 
-@app.command()
-def foo(): ...
+@app.command(help=_("Configure the game."))
+def config(
+    option: Annotated[
+        str,
+        typer.Argument(help=_("The option to configure.")),
+    ],
+):
+    """Configure the game."""
+    raise NotImplementedError(_("This feature is not yet implemented."))
+
+
+@app.command(help=_("Display information about the game."))
+def about():
+    raise NotImplementedError(_("This feature is not yet implemented."))
 
 
 if __name__ == "__main__":
