6852d8966be7835cece1b18ccf5147d1631f8857
Pure <111236541+PureStupid@users.noreply.github.com>
2024-10-02 01:00:13 +0800

    Add tests for main, config and ui manager

---
+++ tests/test__main__.py
@@ -0,0 +1,53 @@
+from unittest.mock import patch
+
+import pytest
+from typer.testing import CliRunner
+
+from undead_slayer.__main__ import app
+from undead_slayer.enums import UI
+
+runner = CliRunner()
+
+
+@pytest.mark.parametrize(
+    "ui",
+    [
+        UI.CLI,
+        UI.TUI,
+        UI.GUI,
+    ],
+)
+class TestMain:
+    def test_play_with_default_ui(self, ui: UI):
+        """Test the `play` command with the default UI.
+
+        Parameters:
+        -----------
+        ui : UI
+            The default user interface.
+        """
+        with (
+            patch("undead_slayer.__main__.get_default_ui") as mock_get_default_ui,
+            patch("undead_slayer.__main__.launch_ui") as mock_launch_ui,
+        ):
+            mock_get_default_ui.return_value = ui
+
+            result = runner.invoke(app, ["play"])
+
+            assert result.exit_code == 0
+            mock_get_default_ui.assert_called_once()
+            mock_launch_ui.assert_called_once_with(ui)
+
+    def test_play_with_specific_ui(self, ui: UI):
+        """Test the `play` command with a specific UI.
+
+        Parameters:
+        -----------
+        ui : UI
+            The specific user interface.
+        """
+        with patch("undead_slayer.__main__.launch_ui") as mock_launch_ui:
+            result = runner.invoke(app, ["play", ui.value])
+
+            assert result.exit_code == 0
+            mock_launch_ui.assert_called_once_with(ui)
--- tests/test_ui_manager.py
+++ tests/test_ui_manager.py
@@ -2,11 +2,30 @@ from unittest.mock import patch
 
 import pytest
 
+from undead_slayer.config.config_manager import ConfigManager
 from undead_slayer.enums import UI
 from undead_slayer.ui.cli.cli import CLI
 from undead_slayer.ui.gui.gui import GUI
 from undead_slayer.ui.tui.tui import TUI
-from undead_slayer.ui.ui_manager import launch_ui
+from undead_slayer.ui.ui_manager import get_default_ui, launch_ui
+from undead_slayer.utils.locations import config_file
+
+
+def test_get_default_ui():
+    """
+    Test the `get_default_ui` function.
+    """
+    with patch.object(
+        ConfigManager, "get_config", return_value=ConfigManager(config_file()).config
+    ) as mock_get_config:
+        mock_get_config.return_value.default_ui = UI.CLI.value
+        assert get_default_ui() == UI.CLI
+
+        mock_get_config.return_value.default_ui = UI.TUI.value
+        assert get_default_ui() == UI.TUI
+
+        mock_get_config.return_value.default_ui = UI.GUI.value
+        assert get_default_ui() == UI.GUI
 
 
 @pytest.mark.parametrize(
